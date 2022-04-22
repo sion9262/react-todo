2bc6a17936e6dbfb92cd478ae917d33dd765aef7
sion <parksion9262@gmail.com>
2022-04-22 17:13:46 +0900

    Add: Change ToDoCategory
    - 카테고리 버튼 클릭 시 카테고리 변경 기능 추가.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,24 @@
-import { IToDo } from "../atoms/atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms/atoms";
 
-const ToDo = ({text}: IToDo) => {
+const ToDo = ({id, text, category}: IToDo) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { currentTarget: {name} } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex(toDos => toDos.id === id);
+      console.log(targetIndex);
+      const newToDo = {text, id, category: name as any};
+      return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex + 1)]
+    });
+  }
 	return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && <button name="DOING" onClick={onClick}>DOING</button>}
+      {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
+      {category !== "DONE" && <button name="DONE" onClick={onClick} >DONE</button>}
     </li>
 	)
 }
